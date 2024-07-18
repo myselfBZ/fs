7ed7891cea3c2e416b18fde11850f133c3ce7c10
Boburmirzo <symonbobur@gmail.com>
2024-07-18 11:48:44 +0500

    Bug fix

--- stack.go
+++ stack.go
@@ -1,13 +1,24 @@
 package main
 
+import "errors"
+
 type Stack struct{
     elements []string
 }
+
+var (
+    Empty = errors.New("Stack is empty")
+)
+
 
 func (s *Stack) Append(e string)  {
     s.elements = append(s.elements, e)
 }
 
-func (s *Stack) Pop(){
-    s.elements = s.elements[len(s.elements)-1:]
+func (s *Stack) Pop() error {
+    if len(s.elements) == 0{
+        return Empty 
+    }
+    s.elements = s.elements[:len(s.elements) -1]
+    return nil
 }
